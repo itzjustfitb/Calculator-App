2492ef2aa354f16b547956ed1f0e3d90d6b6dbf5
Fakhri <fakhrimg@code.edu.az>
2023-09-18 18:39:43 +0400

    done

--- index.js
+++ index.js
@@ -6,6 +6,47 @@ let error_modal_container = document.getElementById("error_modal_container");
 let body = document.body;
 let calculator_container = document.getElementById("calculator_container");
 
+let first_text = document.getElementById("first_text");
+let second_text = document.getElementById("second_text");
+let wrapper = document.getElementById("wrapper");
+let welcoming = document.getElementById("welcoming");
+let calculator = document.getElementById("calculator");
+
+function textEffects() {
+  setTimeout(() => {
+    first_text.style.top = "40%";
+    second_text.style.bottom = "40%";
+    setTimeout(() => {
+      first_text.style.left = "-1000px";
+      second_text.style.right = "-1000px";
+    }, 1500);
+    setTimeout(() => {
+      loadingScreen();
+    }, 2000);
+  }, 2000);
+  setTimeout(() => {
+    wrapper.style.display = "none";
+    welcoming.style.display = "none";
+    showCalculator();
+  }, 7000);
+}
+
+function loadingScreen() {
+  wrapper.style.visibility = "visible";
+  wrapper.style.opacity = "1";
+  setTimeout(() => {
+    wrapper.style.visibility = "hidden";
+    wrapper.style.opacity = "0";
+  }, 2000);
+}
+
+function showCalculator() {
+  calculator.style.visibility = "visible";
+  calculator.style.opacity = "1";
+}
+
+textEffects();
+
 function calculate(number) {
   result.value += number;
 }
